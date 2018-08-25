f95d78ec0341aece00701372a58eb153cea714a9
Chris W <christopherkwildenradt@gmail.com>
2018-08-25 13:36:29 -0700

    Render saved crawls information

--- client/components/SavedCrawls.jsx
+++ client/components/SavedCrawls.jsx
@@ -5,20 +5,32 @@ export default class SavedCrawls extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            crawls: []
         }
     }
 
     componentDidMount() {
         axios.get('/user/pubcrawl/chris')
-        .then(res => console.log(res))
+        .then(res => {
+            console.log(res)
+            this.setState({ crawls: res.data })
+        })
         .catch(err => console.log(err))
     }
 
     render() {
+        const { crawls } = this.state;
         return (
             <div>
-                
+                {crawls.map((crawl, index) => crawl.pubCrawl.map(business => <Business info={business} index={index}/>))}
             </div>
         )
     }
 }
+
+const Business = ({ info, index }) => (
+    <div>PubCrawl#{index}
+        <div>{info.name}</div>
+        <div><img className="businessImage" src={info.image_url} /></div>
+    </div>
+)
--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) =>{
         "business_name": "garaje-san-francisco",
         "location": "san francisco, ca"
     }
-}`
+}`;
 
   app.get('/yelp/:searchQuery', (req, res) => {
     const searchRequest = {
