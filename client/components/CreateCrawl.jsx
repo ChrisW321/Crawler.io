d901d1a46c055e1f176d79e73ccc5d5b45228fed
Chris W <christopherkwildenradt@gmail.com>
2018-08-25 01:53:26 -0700

    Add styles to createCrawl and loaders for CSS modules

--- client/components/CreateCrawl.jsx
+++ client/components/CreateCrawl.jsx
@@ -1,13 +1,14 @@
 import { Component } from 'react';
 import axios from 'axios';
-
+import styles from './CreateCrawl.css';
 
 export default class CreateCrawl extends Component {
     constructor(props) {
         super(props);
         this.state = {
             businesses: [],
-            input: ''
+            input: '',
+            crawl: [],
         }
     }
 
@@ -18,7 +19,7 @@ export default class CreateCrawl extends Component {
     searchYelp(input) {
         axios.get(`/yelp/${input}`)
         .then(res => {
-            console.log(res)
+            console.log(res.data)
             this.setState({businesses: res.data.jsonBody.businesses})
             console.log(this.state)
         })
@@ -26,35 +27,49 @@ export default class CreateCrawl extends Component {
     }
 
     changeInput(e) {
-        console.log(e.which)
-        if (e.which === 13) {
-            this.handleSubmit()
-        }
-        this.setState({input: e.target.value})
-        console.log(this.state.input)
+        e.which === 13 && this.handleSubmit()
+        this.setState({ input: e.target.value })
     }
 
     handleSubmit() {
         this.searchYelp(this.state.input);
-        this.setState({input: ''})
+        this.setState({ input: '' })
+    }
+
+    addToCrawl(info) {
+        console.log('addtocrawl called')
+        this.state.crawl.push(info);
+        this.setState({ crawl: this.state.crawl })
+        console.log(this.state.crawl)
+    }
+
+    removeFromCrawl() {
+        console.log('remove called')
     }
 
     render() {
-        const { businesses } = this.state
+        const { businesses, crawl } = this.state
         return (
             <div>
                 <div>
                     <input type="text" onKeyUp={(e) => this.changeInput(e)}/><button onClick={() => this.handleSubmit()}>Search</button>
                 </div>
-                {businesses.map(business => <Business info={business}/>)}
+                <div>
+                    <div className="twoHalvesInline">
+                        {businesses.map(business => <Business info={business} addToCrawl={this.addToCrawl.bind(this)} />)}
+                    </div>
+                    <div className="twoHalvesInline" id="newCrawlList">
+                        {crawl.map(business => <Business info={business} removeFromCrawl={this.removeFromCrawl.bind(this)} />)}
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-const Business = ({ info }) => (
-    <div>
+const Business = ({ info, addToCrawl }) => (
+    <div onClick={() => addToCrawl(info)}>
         <div>{info.name}</div>
-        <div><img src={info.image_url} /></div>
+        <div><img className="businessImage" src={info.image_url} /></div>
     </div>
 )
