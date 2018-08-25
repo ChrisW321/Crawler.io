5da2164c4e786850dc76cb68153b5ec5111550af
Chris W <christopherkwildenradt@gmail.com>
2018-08-24 18:12:35 -0700

    Modify webpack for css modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/public');
-
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
-  output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?$/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
+    context: __dirname + '/client',
+    entry: './index.jsx',
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015', 'env']
+          },
+        },      
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
-      }
-    ]
-  }
-};
+      ],
+      
+    },
+    output: {
+      path: __dirname + '/public',
+      filename: 'bundle.js',
+    }
+  };
