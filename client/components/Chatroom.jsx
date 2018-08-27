0e83042854da162b1f5cfc4957d811cc330a7721
Chris W <christopherkwildenradt@gmail.com>
2018-08-27 15:20:09 -0700

    Style Chatroom and fillout socket receivers

--- client/components/Chatroom.jsx
+++ client/components/Chatroom.jsx
@@ -9,13 +9,41 @@ export default class Chatroom extends Component {
                 username: 'Hanjoon',
                 msg: 'This crawl is lit Brah!!!'
             }],
+            msg: '',
         }
+        this.socket = io();
+    }
+
+    componentDidMount() {
+        this.socket.on('chat sent', (data) => {
+            this.state.chats.push( data );
+            console.log(this.state.chats);
+            this.setState({ chats: this.state.chats })
+        });
+    }
+
+    updateMsg(e) {
+        if (e.which === 13) {
+            this.sendMsg();
+            this.setState({ msg: ''})
+        } else {
+            this.setState({ msg: e.target.value })
+        }
+    }
+
+    sendMsg() {
+        console.log('chat sent called')
+        this.socket.emit('chat sent', { username: this.props.user, msg: this.state.msg })
+        this.setState({ msg: '' })
     }
 
     render() {
         return (
             <div className="chatroomContainer">
-                <div className="chatroomHeader">Chat Room</div>
+                <div className="chatroomHeader">
+                    Chat Room 
+                    <input type="text" onKeyUp={(e) => this.updateMsg(e)}/>
+                    <button onClick={() => this.sendMsg()}>Send It!</button></div>
                 {this.state.chats.map(chat => <Chat chat={chat}/> )}
             </div>
         )
