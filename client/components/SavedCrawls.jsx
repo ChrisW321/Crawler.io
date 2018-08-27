4f884b0d3da4986e8996e9fa44afaf45117e4828
Chris W <christopherkwildenradt@gmail.com>
2018-08-26 19:46:14 -0700

    Add crawlsaved socket event receiver

--- client/components/SavedCrawls.jsx
+++ client/components/SavedCrawls.jsx
@@ -1,15 +1,22 @@
 import { Component } from 'react';
 import axios from 'axios';
+import styles from './SavedCrawls.css';
 
 export default class SavedCrawls extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            crawls: []
+            crawls: [],
         }
     }
 
     componentDidMount() {
+        const socket = io();
+        socket.on('crawl saved', (data) => {
+            this.state.crawls.push({pubCrawl: data});
+            console.log(this.state.crawls);
+            this.setState({ crawls: this.state.crawls })
+        });
         axios.get('/user/pubcrawl/chris')
         .then(res => {
             console.log(res)
@@ -22,15 +29,30 @@ export default class SavedCrawls extends Component {
         const { crawls } = this.state;
         return (
             <div className="SavedCrawlsContainer">
-                {crawls.map((crawl, index) => crawl.pubCrawl.map(business => <Business info={business} index={index}/>))}
+                {crawls.reverse().map((crawl, index) => {
+                    return (
+                        <div>
+                            <span className="textInlineWithImage">#{index}&nbsp;&nbsp;</span>
+                            {crawl.pubCrawl.map(business => <Business info={business} />)}
+                        </div>
+                    )
+                })}
             </div>
         )
     }
 }
 
-const Business = ({ info, index }) => (
-    <div>PubCrawl#{index + 1}
-        <div>{info.name}</div>
-        <div><img className="businessImage" src={info.image_url} /></div>
-    </div>
+const Business = ({ info }) => (
+    <span>
+        <div className="inlineBlock">
+                <div className="textInlineWithImage">{info.name}&nbsp;</div>
+        </div>
+        <div className="inlineBlock">
+            <img className="businessImage" src={info.image_url} /> 
+        </div>
+        <div className="inlineBlock">
+        <div className="textInlineWithImage">=></div>
+        </div>
+
+    </span>
 )
